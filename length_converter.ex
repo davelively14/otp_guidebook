42d38758ea01a1f56e0bbce45762e24fca159511
Dave Lively <davelively14@gmail.com>
2016-09-07 21:03:49 -0400

    Completed through 2.4

--- length_converter.ex
+++ length_converter.ex
@@ -1,5 +1,5 @@
-defmodule MeterToFeetConverter do
-  def convert(m) do
-    m * 3.28084
-  end
+defmodule MeterToLength do
+  def convert(:foot, m) when is_number(m) and m >= 0, do: m * 3.28084
+  def convert(:inch, m) when is_number(m) and m >= 0, do: m * 39.3701
+  def convert(:yard, m) when is_number(m) and m >= 0, do: m * 1.09361
 end
